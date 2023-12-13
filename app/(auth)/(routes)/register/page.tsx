0b30ae0a28e454a29d0a2c043bb27a91aaf75d2c
Devansh Behl <heydevanshbehl@gmail.com>
2023-12-14 03:11:54 +0530

    initial commit

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      register page
+    </div>
+  )
+}
+
+export default register
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-red-500 text-white">click me</Button>
+    </div>
+  );
+}
