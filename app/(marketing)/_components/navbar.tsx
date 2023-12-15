3242c465cdcac189f946ef9d13bacd7241b69565
Devansh Behl <heydevanshbehl@gmail.com>
2023-12-16 02:58:08 +0530

    added authentication using clerk

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,8 +5,14 @@ import {cn} from '@/lib/utils';
 import React from 'react'
 import { Logo } from './logo';
 import { ModeToggle } from '@/components/mode-toggle';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton} from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
+import Link from 'next/link';
 
 const navbar = () => {
+    const {isAuthenticated,isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
   return (
     <div className={cn(
@@ -15,6 +21,31 @@ const navbar = () => {
     )}>
         <Logo/>
         <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+          {isLoading && (
+            <Spinner/>
+          )}
+          {!isAuthenticated && !isLoading && (
+            <>
+            <SignInButton mode='modal'>
+              <Button variant= "ghost" size="sm">Log In</Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button variant= "ghost" size="sm">Join Notify</Button>
+            </SignInButton>
+            </>
+          )}
+          {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">
+                Enter Notify
+              </Link>
+            </Button>
+            <UserButton
+              afterSignOutUrl="/"
+            />
+          </>
+        )}
         <ModeToggle/>
         </div>
       
