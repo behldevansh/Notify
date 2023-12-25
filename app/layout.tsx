ec87ae9da2e72eef69710abf7471cabb4f92e49b
Devansh Behl <heydevanshbehl@gmail.com>
2023-12-26 02:56:41 +0530

    added apis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
-import type { Metadata } from 'next'
+import { Toaster } from "sonner";
 import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-providers'
+import type { Metadata } from 'next'
+
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,16 +39,21 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-        <ThemeProvider 
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-        storageKey='notify-theme'>
-          {children}
-          </ThemeProvider>
-          </ConvexClientProvider>
-        </body>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   )
 }
