1f56940234cab1d729dbbc846264e7cd6135284d
Devansh Behl <heydevanshbehl@gmail.com>
2024-07-09 14:45:03 +0530

    infinite notes recursive call added

--- app/(primary)/_components/item.tsx
+++ app/(primary)/_components/item.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
-import { ChevronRight, ChevronsUpDown, LucideIcon } from "lucide-react";
+import { ChevronRight, ChevronsUpDown, LucideIcon, Plus } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -31,11 +35,36 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
+
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     event.stopPropagation();
     onExpand?.();
+  };
+
+
+  const onCreate = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise= create({title:"Untitled",parentDocument:id})
+    .then((documentId)=>{
+      if(!expanded){
+        onExpand?.();
+      }
+      // router.push(`/documents/${documentId}`);
+    })
+    toast.promise(promise,{
+      loading: "Creating a new Note ...",
+      success: "New Note created successfully :)",
+      error: "Failed to create a new Note :(",
+    })
+
   };
 
   const ChevronIcon = expanded ? ChevronsUpDown : ChevronRight;
@@ -72,6 +101,17 @@ export const Item = ({
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">CTRL</span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div 
+          role="button"
+          onClick={onCreate}
+          className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+            <Plus className="h-4 w-4 text-muted-foreground"/>
+          </div>
+
+        </div>
       )}
     </div>
   );
