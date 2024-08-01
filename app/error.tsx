37625132224f51a475d227c4547cf9dd5ff7ad4d
Devansh Behl <heydevanshbehl@gmail.com>
2024-08-02 02:17:37 +0530

    fix:shared note once unshared image bug

--- app/error.tsx
+++ app/error.tsx
@@ -8,23 +8,9 @@ import { Button } from "@/components/ui/button";
 const Error = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4">
-      <Image
-        src="/error.png"
-        height="300"
-        width="300"
-        alt="error"
-        className="dark:hidden"
-      />
-      <Image
-        src="/error-dark.png"
-        height="300"
-        width="300"
-        alt="error"
-        className="hidden dark:block"
-      />
-      <h2 className="text-xl font-medium">Something went wrong!</h2>
+      <h2 className="text-xl font-medium">Something went wrong !</h2>
       <Button asChild>
-        <Link href="/documents">Go back</Link>
+        <Link href="/documents">Back to Home?</Link>
       </Button>
     </div>
   );
