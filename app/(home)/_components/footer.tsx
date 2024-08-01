8ee1c2e41e4f4323b840b5ba4c659c7d3d1e5a9b
Devansh Behl <heydevanshbehl@gmail.com>
2024-08-02 02:33:26 +0530

    fix: githubcorner

--- app/(home)/_components/footer.tsx
+++ app/(home)/_components/footer.tsx
@@ -6,19 +6,6 @@ import GithubCorner from "."
 
 export const Footer = () => {
   return (
-    // <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
-    //   {/* <Logo /> */}
-    //   {/* <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-    //     <Button variant="ghost" size="sm">
-    //       Privacy Policy
-    //     </Button>
-    //     <Button variant="ghost" size="sm">
-    //       Terms & Conditions
-    //     </Button>
-    //   </div> */}
-    //   {/* <GithubCorner url='https://github.com/behldevansh/IEEE_Algoverse'/> */}
-
-    // </div>
-    <GithubCorner url='https://github.com/behldevansh/IEEE_Algoverse'/>
+    <GithubCorner url='https://github.com/behldevansh/Notify'/>
   )
 }
